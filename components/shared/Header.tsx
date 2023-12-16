71d1327869da514064d28b7dcfc119a511941ebc
nadeeraka <global-nadeerakan@gmail.com>
2023-12-16 21:48:28 +0530

    hero section update

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
-import React from "react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import Logo from "../../public/c.png";
-import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
+import Logo from "../../public/c.png";
 
 const Header = () => {
   return (
-    <div className="w-full border-b">
-      <div className="wrapper flex justify-between items-center">
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
         <Link href={"/"} className="w-36">
           <div className="flex items-center">
             <Image src={Logo} alt="logo" width={50} height={50} />
@@ -19,14 +20,14 @@ const Header = () => {
 
         <SignedIn>
           <nav className="md:flex-between hidden w-full max-w-xs">
-            {/* <NavItems /> */}
+            <NavItems />
           </nav>
         </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
-            {/* <MobileNav /> */}
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
@@ -35,7 +36,7 @@ const Header = () => {
           </SignedOut>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
